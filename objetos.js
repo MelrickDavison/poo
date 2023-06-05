717922a4e481d7ed20b21b0ec3949bdd2ce9a700
MelrickDavison <115827008+MelrickDavison@users.noreply.github.com>
2023-06-05 12:03:25 -0300

    Add files via upload

---
+++ objetos.js
@@ -0,0 +1,18 @@
+let aluno0 = new Aluno("Fabricio", 16);
+let aluno1 = new Aluno("Gabriel", 17);
+let aluno2 = new Aluno ("Melrick", 16);
+let aluno3 = new Aluno("Kemylly", 16);
+let aluno4 = new Aluno("Emilly", 15);
+
+let segundao = new Turma();
+let terceirao = new Turma();
+segundao.adicionarAluno = aluno0;
+segundao.adicionarAluno = aluno1;
+segundao.adicionarAluno = aluno2;
+terceirao.adicionarAluno = aluno3;
+terceirao.adicionarAluno = aluno4;
+
+segundao.listaDeAlunos[2] = ""
+console.log(terceirao)
+console.log(segundao)
+
