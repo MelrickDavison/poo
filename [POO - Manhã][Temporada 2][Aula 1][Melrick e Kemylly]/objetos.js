7dd03723211742ee6653239f857ccd6579e64625
MelrickDavison <126255280+vitor-gss@users.noreply.github.com>
2023-08-08 10:05:24 -0300

    criando array

--- [POO - Manhã][Temporada 2][Aula 1][Melrick e Kemylly]/objetos.js
+++ [POO - Manhã][Temporada 2][Aula 1][Melrick e Kemylly]/objetos.js
@@ -1,3 +1,9 @@
 let aluno1 = new Aluna("Fabricio", 16, 101);
 let aluno2 = new Aluna("Melrick", 16, 102);
 let aluno3 = new Aluna("Kemylly", 17, 103);
+
+let listaDeAlunos = [];
+listaDeAlunos.push(aluno1);
+listaDeAlunos.push(aluno2);
+listaDeAlunos.push(aluno3);
+ 
