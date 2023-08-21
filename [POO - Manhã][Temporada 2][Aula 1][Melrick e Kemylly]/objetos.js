53844aeee8c9ac3fdab9bb41b8f2cde743ea0b90
jhonattanRamon <115827008+MelrickDavison@users.noreply.github.com>
2023-08-21 10:36:13 -0300

    colocando imagens

--- [POO - Manhã][Temporada 2][Aula 1][Melrick e Kemylly]/objetos.js
+++ [POO - Manhã][Temporada 2][Aula 1][Melrick e Kemylly]/objetos.js
@@ -1,12 +1,12 @@
-let alune1 = new Alunes("Fabricio", 16, 101, '');
-let alune2 = new Alunes("Melrick", 16, 102, '');
-let alune3 = new Alunes("Kemylly", 17, 103);
+let alune1 = new Alunes("Fabricio", 16, 101, 'https://lh3.googleusercontent.com/d/17jAtucgnXEUVT06_Rhm-n1xQazWmsuM5');
+let alune2 = new Alunes("Melrick", 16, 102, 'https://lh3.googleusercontent.com/d/1XUZw8EtjRqL7ouVNORZIVGii2FJxxL_F ');
+let alune3 = new Alunes("Kemylly", 17, 103, 'https://lh3.googleusercontent.com/d/1uWL4Z7JRW478UM_pObd4dGNp--USG5or');
 
 let listaDeAlunes = [];
 listaDeAlunes.push(alune1);
 listaDeAlunes.push(alune2);
 listaDeAlunes.push(alune3);
- document.getElementById("text").innerHTML = ""
+document.getElementById("text").innerHTML = ""
 for (let i = 0; i < listaDeAlunes.length; i++) {
 
     document.getElementById("text").innerHTML += (
@@ -18,5 +18,5 @@ for (let i = 0; i < listaDeAlunes.length; i++) {
          </div>
     
     `);
-    
+
 }
