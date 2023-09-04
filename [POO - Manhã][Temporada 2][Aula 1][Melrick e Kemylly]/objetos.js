d1060926fdebf01ecdc376b716a2ece83a7bb95b
MelrickDavison <115827008+MelrickDavison@users.noreply.github.com>
2023-09-04 10:02:34 -0300

    add objects

--- [POO - Manhã][Temporada 2][Aula 1][Melrick e Kemylly]/objetos.js
+++ [POO - Manhã][Temporada 2][Aula 1][Melrick e Kemylly]/objetos.js
@@ -1,11 +1,26 @@
-let alune1 = new Alunes("912A", "Fabricio Arcanjo Guimarães Costa", 16, 101, 'https://lh3.googleusercontent.com/d/17jAtucgnXEUVT06_Rhm-n1xQazWmsuM5', 'fagc1@aluno.ifal.edu.br');
-let alune2 = new Alunes("912A","Melrick Davison Fonseca Ribeiro", 16, 102, 'https://lh3.googleusercontent.com/d/1XUZw8EtjRqL7ouVNORZIVGii2FJxxL_F ', "mdfr1@aluno.ifal.edu.br");
+let alune0 = new Alunes("912A","Helysson Daniel Ferreira da Silva", 17, 100, 'https://lh3.googleusercontent.com/d/1IVyT8vEVgMC7UoKOcgqo3Nol9tBSmSLs', "hdfs1@aluno.ifal.edu.br");
+let alune1 = new Alunes("912A", "Fabricio Arcanjo Guimarães Costa", 17, 101, 'https://lh3.googleusercontent.com/d/17jAtucgnXEUVT06_Rhm-n1xQazWmsuM5', 'fagc1@aluno.ifal.edu.br');
+let alune2 = new Alunes("912A","Melrick Davison Fonseca Ribeiro", 17, 102, 'https://lh3.googleusercontent.com/d/1XUZw8EtjRqL7ouVNORZIVGii2FJxxL_F ', "mdfr1@aluno.ifal.edu.br");
 let alune3 = new Alunes("912A","Kemylly Gabrielly Gomes Rodrigues", 17, 103, 'https://lh3.googleusercontent.com/d/1uWL4Z7JRW478UM_pObd4dGNp--USG5or', "kggr1@aluno.ifal.edu.br");
+let alune4 = new Alunes("912A","Ellen Jayani dos Santos Viana", 17, 104, 'https://lh3.googleusercontent.com/d/1xkGio1rSh25XTFnKAHcwXXsFliwU22HQ', "ejsv1@aluno.ifal.edu.br");
+let alune5 = new Alunes("912A","Gabriel Luiz Lourenço Farias", 17, 105, 'https://lh3.googleusercontent.com/d/1NTR45eSWi_6haSJi3P-IMqGJdkrOt-0J', "gllf1@aluno.ifal.edu.br");
+let alune6 = new Alunes("912A","Anthony Victor da Silva Machado", 18, 106, 'https://lh3.googleusercontent.com/d/1eynFc-CNmGcYZb6b5r5QxWs_DINsmkFT', "avsm1@aluno.ifal.edu.br");
+let alune7 = new Alunes("912A","Thiago Rogério de Melo", 17, 107, 'https://lh3.googleusercontent.com/d/1yK1BWGSRxuHt4gP6mmYNOIRHs4plJ8gR', "trm@aluno.ifal.edu.br");
+let alune8 = new Alunes("912A","Wytalo Mikael Reis dos Santos", 17, 108, 'https://lh3.googleusercontent.com/d/1veLv6ByeW7wDjrojoeoYjeE-vIprjSLz', "wmrs1@aluno.ifal.edu.br");
+let alune9 = new Alunes("912A","Girleide de Araújo Silva", 17, 109, 'https://lh3.googleusercontent.com/d/1D0tK367ziUWFUs2PgIVZKbTHEuiPV43r', "gas1@aluno.ifal.edu.br");
 
 let listaDeAlunes = [];
+listaDeAlunes.push(alune0);
 listaDeAlunes.push(alune1);
 listaDeAlunes.push(alune2);
 listaDeAlunes.push(alune3);
+listaDeAlunes.push(alune4);
+listaDeAlunes.push(alune5);
+listaDeAlunes.push(alune6);
+listaDeAlunes.push(alune7);
+listaDeAlunes.push(alune8);
+listaDeAlunes.push(alune9);
+
 document.getElementById("text").innerHTML = ""
 for (let i = 0; i < listaDeAlunes.length; i++) {
 
