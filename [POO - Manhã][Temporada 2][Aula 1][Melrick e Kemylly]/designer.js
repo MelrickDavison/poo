472a475a82f1562cb80262e124b5151b4c5a3c2d
jhonattanRamon <115827008+MelrickDavison@users.noreply.github.com>
2023-09-12 09:02:59 -0300

    Update html and create files

---
+++ [POO - Manhã][Temporada 2][Aula 1][Melrick e Kemylly]/designer.js
@@ -0,0 +1,43 @@
+
+document.getElementById("text").innerHTML = ""
+for (let i = 0; i < listaDeAlunes.length; i++) {
+
+    document.getElementById("text").innerHTML += (
+        `<div class='card'> 
+        <div class='cabecalho'>
+            <div>
+                <p class='turma'>
+             Turma: ${listaDeAlunes[i].turma}
+             </p>
+                <h3 class='nome'>${listaDeAlunes[i].getNome()}</h3>
+             </div>
+            <img class='logoifal' src='img/ifal.png'>
+        </div>
+
+        <div class='conteudo'>
+        <img class='foto' src='${listaDeAlunes[i].foto}'>
+         <div class='info'>
+              <div>
+                 <p class='label'> Idade:</p>
+                 <p class='valor's>  ${listaDeAlunes[i].getIdade()}  </p>
+              </div>
+              <div>
+                    <p class='label'> Matricula:</p>
+                    <p class='valor's>  ${listaDeAlunes[i].matricula}  </p>
+                </div>
+                <div>
+                    <p class='label'> Email:  </p>
+                    <p class='valor'> ${listaDeAlunes[i].email} </p>
+                </div>
+         </div>
+        </div>
+
+      
+           
+
+
+         </div>
+    
+    `);
+
+}
