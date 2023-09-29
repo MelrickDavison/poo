32cb89e8691e19c1cec81b87f73b43d21c001122
jhonattanRamon <115827008+MelrickDavison@users.noreply.github.com>
2023-09-29 11:00:43 -0300

    New metod

--- [POO - Manhã][Temporada 2][Atividade Estoque][Melrick e Kemylly]/classes.js
+++ [POO - Manhã][Temporada 2][Atividade Estoque][Melrick e Kemylly]/classes.js
@@ -10,22 +10,15 @@ class Estoque {
         this.listaDeProdutos = [];
     }
 
-      adicionarProduto(produto) {
+    adicionarProduto(produto) {
         this.listaDeProdutos.push(produto);
     }
-    calcularValorTotal(){
-
+    calcularValorTotal() {
         var total = 0;
-
-
-
-        for(let i = 0; i < this.listaDeProdutos.length; i++){
+        for (let i = 0; i < this.listaDeProdutos.length; i++) {
 
             total += this.listaDeProdutos[i].quantidade * this.listaDeProdutos[i].precoUnitario;
-
         }
-
         return total;
-
     }
 }
