ffed0554315b7d5550a831fd8dafa338fb48c3fa
MelrickDavison <115827008+MelrickDavison@users.noreply.github.com>
2023-09-29 09:38:13 -0300

    Update classes.js

--- [POO - Manhã][Temporada 2][Atividade Estoque][Melrick e Kemylly]/classes.js
+++ [POO - Manhã][Temporada 2][Atividade Estoque][Melrick e Kemylly]/classes.js
@@ -13,4 +13,19 @@ class Estoque {
       adicionarProduto(produto) {
         this.listaDeProdutos.push(produto);
     }
+    calcularValorTotal(){
+
+        var total = 0;
+
+
+
+        for(let i = 0; i < this.listaDeProdutos.length; i++){
+
+            total += this.listaDeProdutos[i].quantidade * this.listaDeProdutos[i].precoUnitario;
+
+        }
+
+        return total;
+
+    }
 }
