a4433b68bf2dd9666807aea00aca334be9e414cf
MelrickDavison <115827008+MelrickDavison@users.noreply.github.com>
2023-10-21 11:10:43 -0300

    update

--- [POO - Manhã][Temporada][Aula Sábado][Melrick e Fabricio]/world.js
+++ [POO - Manhã][Temporada][Aula Sábado][Melrick e Fabricio]/world.js
@@ -1,3 +1,5 @@
+
+
 //Parede
 function parede(x, y, width, height) {
     return Matter.Bodies.rectangle(x, y, width, height, {
@@ -60,3 +62,21 @@ let runner = Matter.Runner.create();
 Matter.Runner.run(runner, engine);
 // ===================================================
 
+//Construção das 4 paredes
+Matter.World.add(engine.world, [
+    parede(280, 0, 2000, 20), //top
+    parede(280, 800, 2000, 20), //bottom
+    parede(0, 400, 20, 1024),   // left
+    parede(1024, 400, 20, 1024), // right
+])
+
+//Criação dos players
+let bala = new Bala(5, '#993322');
+let bala1 = new Bala(10, "#aa88aa");
+let player = new Player('player1', 8, 40, '#33221739', 30, 10, bala);
+let player2 = new Player('player2', 4, 40, '#98020245', 30, 10, bala1);
+
+// Posicionamento dos players na tela
+
+Matter.World.add(engine.world, playerVisual(player, 90, 330));
+Matter.World.add(engine.world, playerVisual(player2, 900, 330));
