8091fb069cb5d337422d1c5a8da60f5cc020af2f
MelrickDavison <115827008+MelrickDavison@users.noreply.github.com>
2023-10-31 10:28:24 -0300

    update

---
+++ Tela de login/controladorLogin.js
@@ -0,0 +1,8 @@
+const form = document.getElementById("form");
+const email = document.getElementById("email");
+const senha = document.getElementById("senha");
+const btEntrar = document.getElementById("btEntrar");
+
+btEntrar.addEventListener("click", ()=>{
+    window.location.href = "lista.html"
+})
